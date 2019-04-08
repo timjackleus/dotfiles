25aa2d3acf3f3416b50fcebf50e742615dfb866a
timkivra <tim@kivra.com>
2019-04-08 21:10:51 +0200

    add neovim to brew.sh

--- brew.sh
+++ brew.sh
@@ -17,9 +17,10 @@ brew install openssh
 brew install screen
 
 # Install other useful binaries.
+brew install cmake
 brew install ctags
 brew install git
-brew install macvim --with-override-system-vim
+brew install neovim
 brew install node # This installs 'npm' too using the recommended installation method
 brew install npm
 brew install nvm
