43cb11c76a5e2ab1e78fc98bac07c9d348fa8a64
timjackleus <timjackleus@gmail.com>
2020-01-17 22:08:08 +0100

    feat(brew): swap 1password with enpass

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,15 +1,15 @@
 # Install native apps
 
 # daily
-brew cask install 1password6
 brew cask install alfred
 brew cask install dropbox
-brew cask install notion
+brew cask install enpass
 brew cask install franz
+brew cask install google-backup-and-sync
 brew cask install google-chrome
+brew cask install notion
 brew cask install slack
 brew cask install spotify
-brew cask install google-backup-and-sync
 
 # dev
 brew cask install docker
