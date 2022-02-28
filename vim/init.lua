931827a4afd2aa3687781bbca9af9027fe36049c
timjackleus <timjackleus@gmail.com>
2022-02-28 08:51:30 +0100

    fix: cleanup

--- vim/init.lua
+++ vim/init.lua
@@ -12,38 +12,27 @@ require('lualine-config')
 local use = require('packer').use
 
 return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+  use { 'wbthomason/packer.nvim' }
+	use { 'mhinz/vim-startify' }
+  use { 'nvim-treesitter/nvim-treesitter' }
+  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+  use { 'tpope/vim-commentary' }
+  use { 'christoomey/vim-system-copy' }
+  use { 'JoosepAlviste/nvim-ts-context-commentstring' }
+	use { 'tpope/vim-surround' }
+	use { "catppuccin/nvim", as = "catppuccin" }
+  use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
+  use { 'windwp/nvim-spectre', requires = { {'nvim-lua/plenary.nvim'} } }
   use {
 		'neoclide/coc.nvim',
 		branch = 'release',
 		run = ':CocInstall coc-css coc-eslint coc-explorer coc-go coc-highlight coc-html coc-json coc-lua coc-pairs coc-prettier coc-snippets coc-svelte coc-tsserver coc-yaml coc-emmet'
 	}
-	use({
-	"catppuccin/nvim",
-		as = "catppuccin"
-	})
-	use 'mhinz/vim-startify'
-  use 'nvim-treesitter/nvim-treesitter'
-  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
-  use 'tpope/vim-commentary'
-  use 'christoomey/vim-system-copy'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
   use {
     'nvim-telescope/telescope.nvim',
     	requires = {
 				{'nvim-lua/plenary.nvim'},
 				{ 'nvim-telescope/telescope-github.nvim' }
-		}
-  }
-	-- use { 'echasnovski/mini.nvim', branch = 'stable' }
-  use {
-    'windwp/nvim-spectre',
-    	requires = {
-				{'nvim-lua/plenary.nvim'}
 		}
   }
 end)
