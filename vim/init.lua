ca17e271cb24d46b21a8a55fa522020eadecedb7
timjackleus <timjackleus@gmail.com>
2022-05-19 10:43:11 +0200

    fix: cleanup

--- vim/init.lua
+++ vim/init.lua
@@ -8,6 +8,7 @@ require('gitblame-config')
 require('treesitter-config')
 require('nvim-tree-config')
 require('lsp-config')
+require('prettier-config')
 
 require('Comment').setup()
 
@@ -24,14 +25,14 @@ return require('packer').startup(function()
     end
 	}
   use { 'christoomey/vim-system-copy' }
-	use {"ellisonleao/glow.nvim"}
+	use { 'ellisonleao/glow.nvim' }
   use { 'JoosepAlviste/nvim-ts-context-commentstring' }
 	use { 'tpope/vim-surround' }
-	use { "catppuccin/nvim", as = "catppuccin" }
+	use { 'catppuccin/nvim', as = 'catppuccin' }
   use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
   use { 'windwp/nvim-spectre', requires = { {'nvim-lua/plenary.nvim'} } }
-	use { "mattn/emmet-vim" }
-	use { "f-person/git-blame.nvim" }
+	use { 'mattn/emmet-vim' }
+	use { 'f-person/git-blame.nvim' }
 	use { 'neovim/nvim-lspconfig' }
   use { 'nvim-telescope/telescope.nvim',
     requires = {
@@ -45,15 +46,15 @@ return require('packer').startup(function()
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
 	}
-	use { "hrsh7th/nvim-cmp",
+	use { 'hrsh7th/nvim-cmp',
 		requires = {
-			"hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp", 'hrsh7th/cmp-nvim-lua', 'hrsh7th/cmp-path', 'hrsh7th/cmp-emoji', 'L3MON4D3/LuaSnip'
+			'hrsh7th/cmp-buffer', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-nvim-lua', 'hrsh7th/cmp-path', 'hrsh7th/cmp-emoji', 'L3MON4D3/LuaSnip'
 		}
 	}
 	use { 'tzachar/cmp-tabnine',
 		run = './install.sh',
 		requires = 'hrsh7th/nvim-cmp'
 	}
-	use('jose-elias-alvarez/null-ls.nvim')
-	use('MunifTanjim/prettier.nvim')
+	use {'jose-elias-alvarez/null-ls.nvim'}
+	use {'MunifTanjim/prettier.nvim'}
 end)
--- vim/lua/treesitter-config.lua
+++ vim/lua/treesitter-config.lua
@@ -1,5 +1,25 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "bash", "css", "dockerfile", "fish", "go", "html", "http", "javascript", "jsdoc", "json", "lua", "python", "rust", "scss", "svelte", "tsx", "typescript", "vim", "yaml" },
+  ensure_installed = {
+		'bash',
+		'css',
+		'dockerfile',
+		'fish',
+		'go',
+		'html',
+		'http',
+		'javascript',
+		'jsdoc',
+		'json',
+		'lua',
+		'python',
+		'rust',
+		'scss',
+		'svelte',
+		'tsx',
+		'typescript',
+		'vim',
+		'yaml'
+	},
   context_commentstring = {
     enable = true
   },
