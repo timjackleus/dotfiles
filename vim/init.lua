a9b707675581ca8aace8652a9e151c44a3e5dd72
timjackleus <timjackleus@gmail.com>
2022-06-18 13:16:41 +0200

    fix: add gitsigns

--- vim/init.lua
+++ vim/init.lua
@@ -9,45 +9,44 @@ require('treesitter-config')
 require('nvim-tree-config')
 require('lsp-installer-config') -- must be called before lsp-config
 require('lsp-config')
+require('gitsigns-config')
 
 require('nvim-autopairs').setup();
+require('Comment').setup()
 
 local use = require('packer').use
 
 return require('packer').startup(function()
-  use { 'wbthomason/packer.nvim' }
+	use { 'wbthomason/packer.nvim' }
 	use { 'mhinz/vim-startify' }
-  use { 'nvim-treesitter/nvim-treesitter' }
-  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
-	use { 'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-	}
-  use { 'christoomey/vim-system-copy' }
+	use { 'nvim-treesitter/nvim-treesitter' }
+	use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+	use { 'numToStr/Comment.nvim' }
+	use { 'christoomey/vim-system-copy' }
 	use { 'ellisonleao/glow.nvim' }
-  use { 'JoosepAlviste/nvim-ts-context-commentstring' }
+	use { 'JoosepAlviste/nvim-ts-context-commentstring' }
 	use { 'tpope/vim-surround' }
 	use { 'catppuccin/nvim', as = 'catppuccin' }
-  use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
-  use { 'windwp/nvim-spectre', requires = { {'nvim-lua/plenary.nvim'} } }
+	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+	use { 'windwp/nvim-spectre', requires = { { 'nvim-lua/plenary.nvim' } } }
 	use { 'mattn/emmet-vim' }
 	use { 'f-person/git-blame.nvim' }
 	use { 'neovim/nvim-lspconfig' }
+	use { 'lewis6991/gitsigns.nvim' }
 	use {
-    "williamboman/nvim-lsp-installer",
+		"williamboman/nvim-lsp-installer",
 	}
-  use { 'nvim-telescope/telescope.nvim',
-    requires = {
-			{'nvim-lua/plenary.nvim'},
+	use { 'nvim-telescope/telescope.nvim',
+		requires = {
+			{ 'nvim-lua/plenary.nvim' },
 			{ 'nvim-telescope/telescope-github.nvim' }
 		}
-  }
+	}
 	use { 'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- optional, for file icon
+		},
+		tag = 'nightly' -- optional, updated every week. (see issue #1193)
 	}
 	use { 'hrsh7th/nvim-cmp',
 		requires = {
@@ -58,7 +57,18 @@ return require('packer').startup(function()
 		run = './install.sh',
 		requires = 'hrsh7th/nvim-cmp'
 	}
-	use {'jose-elias-alvarez/null-ls.nvim'}
+	use { 'jose-elias-alvarez/null-ls.nvim' }
 	use { 'windwp/nvim-autopairs' }
 	use { "lukas-reineke/lsp-format.nvim" }
+	-- use {
+	-- 	"folke/trouble.nvim",
+	-- 	requires = "kyazdani42/nvim-web-devicons",
+	-- 	config = function()
+	-- 		require("trouble").setup {
+	-- 			-- your configuration comes here
+	-- 			-- or leave it empty to use the default settings
+	-- 			-- refer to the configuration section below
+	-- 		}
+	-- 	end
+	-- }
 end)
