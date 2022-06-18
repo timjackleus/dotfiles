f73e7abab133d525b5c81bff6808105e37c29882
timjackleus <timjackleus@gmail.com>
2022-06-18 22:08:03 +0200

    refactor: inspiration from kickstart

--- vim/lua/lsp-config.lua
+++ vim/lua/lsp-config.lua
@@ -1,97 +1,78 @@
-  -- Setup lspconfig.
-local present, cmpNvimLsp = pcall(require, "cmp_nvim_lsp")
+local lspconfig = require 'lspconfig'
+local on_attach = function(_, bufnr)
+	local opts = { buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+	vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+	vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
 
-if not present then
-   return
-end
-
-local capabilities = cmpNvimLsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local null_ls = require('null-ls')
-local lsp_format = require('lsp-format')
-
-lsp_format.setup {
-  javascript = {
-    indent_width = 2,
-    order = { 'eslint', 'null-ls' }
-  },
-  typescript = {
-    indent_width = 2,
-    order = { 'eslint', 'null-ls' }
-  },
-  html = {
-    indent_width = 2
-  }
-}
-
-local on_attach = function (client, bufnr)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
-	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer = 0})
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
 	vim.keymap.set("n", "[g", vim.diagnostic.goto_next, { buffer = 0 })
 	vim.keymap.set("n", "]g", vim.diagnostic.goto_prev, { buffer = 0 })
 	vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
-	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
-
-	lsp_format.on_attach(client)
+	vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
 end
 
-	-- Lua setup
--- brew install lua-language-server
-require'lspconfig'.sumneko_lua.setup {
-	capabilities = capabilities;
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Enable the following language servers
+local servers = { 'svelte', 'tsserver', 'gopls', }
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
+end
+
+require 'lspconfig'.sumneko_lua.setup {
 	on_attach = on_attach;
+	capabilities = capabilities;
 	settings = {
 		Lua = {
 			diagnostics = {
-				globals = {'vim'},
+				globals = { 'vim' },
+			},
+			telemetry = {
+				enable = false,
 			},
 		},
 	},
 }
 
-	-- Null-ls setup
-require'null-ls'.setup({
-  sources = {
-    null_ls.builtins.formatting.prettierd
-  },
-  on_attach = on_attach
+-- Null-ls setup
+require 'null-ls'.setup({
+	debug = true,
+	sources = {
+		require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.diagnostics.eslint,
+		require("null-ls").builtins.completion.spell,
+		require("null-ls").builtins.formatting.prettierd,
+	},
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			-- vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+			vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()"
+		end
+	end,
 })
+-- Setup nvim-cmp.
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
--- tsserver
--- npm install -g typescript typescript-language-server
-require'lspconfig'.tsserver.setup{
-	capabilities = capabilities;
-	on_attach = on_attach;
-}
-
--- Svelte setup
--- npm install -g svelte-language-server
-require'lspconfig'.svelte.setup{
-	capabilities = capabilities;
-	on_attach = on_attach;
-}
-
--- Golang setup
-require'lspconfig'.gopls.setup{
-	capabilities = capabilities;
-	on_attach = on_attach
-}
-
- -- Setup nvim-cmp.
-vim.opt.completeopt = {"menu", "menuone", "noselect"}
-
-local cmp = require'cmp'
-
+local luasnip = require 'luasnip'
+local cmp = require 'cmp'
 cmp.setup({
 	snippet = {
 		expand = function(args)
 			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -99,35 +80,38 @@ cmp.setup({
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		{ name = 'path' },
 		{ name = 'luasnip' },
-	 	{ name = 'buffer', keyword_length = 5  },
+		{ name = 'buffer', keyword_length = 5 },
 	})
 })
 
 -- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-	}, {
-		{ name = 'buffer' },
-	})
-})
-
-
--- local present, lsp = pcall(require, "lsp")
-
--- brew install gopls
-
--- if not present then
---    return
--- end
-
--- lsp.gopls.setup {}
-
--- lsp.setup {
---
--- }
+-- cmp.setup.filetype('gitcommit', {
+-- 	sources = cmp.config.sources({
+-- 		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+-- 	}, {
+-- 		{ name = 'buffer' },
+-- 	})
+-- })
