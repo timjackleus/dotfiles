b485cae2b76e5e572b82a9e6534509b03fadca8b
timjackleus <timjackleus@gmail.com>
2022-10-01 12:32:12 +0200

    chore(vim): change surround plugin for lua

--- vim/init.lua
+++ vim/init.lua
@@ -14,61 +14,69 @@ require("hop-config")
 
 require("nvim-autopairs").setup()
 require("Comment").setup()
+require("nvim-surround").setup()
 
 local use = require("packer").use
 
 return require("packer").startup(function()
-  use({ "wbthomason/packer.nvim" })
-  use({ "mhinz/vim-startify" })
-  use({ "nvim-treesitter/nvim-treesitter" })
-  use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
-  use({ "numToStr/Comment.nvim" })
-  use({ "christoomey/vim-system-copy" })
-  use({ "ellisonleao/glow.nvim" })
-  use({ "JoosepAlviste/nvim-ts-context-commentstring" })
-  use({ "tpope/vim-surround" })
-  use({ "catppuccin/nvim", as = "catppuccin" })
-  use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
-  use({ "windwp/nvim-spectre", requires = { { "nvim-lua/plenary.nvim" } } })
-  use({ "mattn/emmet-vim" })
-  use({ "f-person/git-blame.nvim" })
-  use({ "neovim/nvim-lspconfig" })
-  use({ "lewis6991/gitsigns.nvim" })
-  use({ "williamboman/nvim-lsp-installer" })
-  use({
-    "nvim-telescope/telescope.nvim",
-    requires = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope-github.nvim" },
-    },
-  })
-  use({
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-  })
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-emoji",
-      "L3MON4D3/LuaSnip",
-    },
-  })
-  use({ "saadparwaiz1/cmp_luasnip" })
-  use({ "jose-elias-alvarez/null-ls.nvim" })
-  use({ "windwp/nvim-autopairs" })
-  use({ "lukas-reineke/lsp-format.nvim" })
-  use({ "rafamadriz/friendly-snippets" })
-  use({
-    "phaazon/hop.nvim",
-    branch = "v1", -- optional but strongly recommended
-  })
+	use({ "wbthomason/packer.nvim" })
+	use({ "mhinz/vim-startify" })
+	use({ "nvim-treesitter/nvim-treesitter" })
+	use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
+	use({ "numToStr/Comment.nvim" })
+	use({ "christoomey/vim-system-copy" })
+	use({ "ellisonleao/glow.nvim" })
+	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
+	use({ "windwp/nvim-spectre", requires = { { "nvim-lua/plenary.nvim" } } })
+	use({ "mattn/emmet-vim" })
+	use({ "f-person/git-blame.nvim" })
+	use({ "neovim/nvim-lspconfig" })
+	use({ "lewis6991/gitsigns.nvim" })
+	use({ "williamboman/nvim-lsp-installer" })
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-github.nvim" },
+		},
+	})
+	use({
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	})
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-emoji",
+			"L3MON4D3/LuaSnip",
+		},
+	})
+	use({ "saadparwaiz1/cmp_luasnip" })
+	use({ "jose-elias-alvarez/null-ls.nvim" })
+	use({ "windwp/nvim-autopairs" })
+	use({ "lukas-reineke/lsp-format.nvim" })
+	use({ "rafamadriz/friendly-snippets" })
+	use({
+		"phaazon/hop.nvim",
+		branch = "v1", -- optional but strongly recommended
+	})
+	use({
+		"kylechui/nvim-surround",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	})
 end)
