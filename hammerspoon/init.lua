f37b88f9ec5852084233fabff40d42396c393c44
timjackleus <timjackleus@gmail.com>
2023-08-11 21:08:46 +0200

    feat(hammerspoon): add current window to replace magnet

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,34 +1,49 @@
 -- declare hammerspoon as a global
 local hs = hs
 
+-- remove animations
+hs.window.animationDuration = 0
+
+-- set measurements and position for window
+local function setMeasurements(win, position)
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	if position == "left" then
+		f.x = max.x
+		f.y = max.y
+		f.w = max.w / 2
+		f.h = max.h
+	elseif position == "right" then
+		f.x = max.x + (max.w / 2)
+		f.y = max.y
+		f.w = max.w / 2
+		f.h = max.h
+	elseif position == "center" then
+		f.x = max.x
+		f.y = max.y
+		f.w = max.w
+		f.h = max.h
+	end
+
+	win:setFrame(f)
+end
+
 -- Helper function to position app windows
 local function positionAppWindows(appName, position)
 	local appWindows = hs.appfinder.appFromName(appName):allWindows()
 
 	for _, win in pairs(appWindows) do
-		local f = win:frame()
-		local screen = win:screen()
-		local max = screen:frame()
-
-		if position == "left" then
-			f.x = max.x
-			f.y = max.y
-			f.w = max.w / 2
-			f.h = max.h
-		elseif position == "right" then
-			f.x = max.x + (max.w / 2)
-			f.y = max.y
-			f.w = max.w / 2
-			f.h = max.h
-		elseif position == "center" then
-			f.x = max.x
-			f.y = max.y
-			f.w = max.w
-			f.h = max.h
-		end
-
-		win:setFrame(f)
+		setMeasurements(win, position)
 	end
+end
+
+-- Helper function to position focused window
+local function positionCurrentWindow(position)
+	local win = hs.window.focusedWindow()
+
+	setMeasurements(win, position)
 end
 
 -- Window setup for large monitors
@@ -53,4 +68,16 @@ hs.hotkey.bind({ "alt", "shift", "ctrl" }, "W", function()
 	positionAppWindows("Notion", "center")
 	positionAppWindows("Teams", "center")
 	positionAppWindows("Slack", "center")
+end)
+
+hs.hotkey.bind({ "alt", "shift", "ctrl" }, "H", function()
+	positionCurrentWindow("left")
+end)
+
+hs.hotkey.bind({ "alt", "shift", "ctrl" }, "L", function()
+	positionCurrentWindow("right")
+end)
+
+hs.hotkey.bind({ "alt", "shift", "ctrl" }, "return", function()
+	positionCurrentWindow("center")
 end)
