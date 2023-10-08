759b9f380849be88752fce95db39bbdd682d4944
timjackleus <timjackleus@gmail.com>
2023-10-08 08:21:04 +0200

    fix(vim): disable bufferline and leap again

--- vim/lua/plugins/disabled.lua
+++ vim/lua/plugins/disabled.lua
@@ -1,8 +1,10 @@
 return {
   { "echasnovski/mini.surround", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.animate ", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "SmiteshP/nvim-navic", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "ggandor/leap.nvim", enabled = false },
 }
