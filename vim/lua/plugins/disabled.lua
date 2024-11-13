69088195fee512bb44f0f9f65d04a4271a792f11
timjackleus <timjackleus@gmail.com>
2024-11-13 11:09:42 +0100

    chore(vim): cleanup

--- vim/lua/plugins/disabled.lua
+++ vim/lua/plugins/disabled.lua
@@ -3,7 +3,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.animate ", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
   { "SmiteshP/nvim-navic", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
