45b1e70ce3efcec737b286f456da65b2ac61c2be
timjackleus <timjackleus@gmail.com>
2024-12-19 10:27:58 +0100

    feat(vim): use ghost text instead of list for ai completion

--- vim/lua/config/options.lua
+++ vim/lua/config/options.lua
@@ -1,5 +1,3 @@
-vim.g.gitblame_enabled = 0
-
 -- avoid root directory suddenly change
 vim.g.root_spec = { "cwd" }
 
@@ -8,6 +6,10 @@ vim.opt.hidden = true
 vim.opt.swapfile = false
 vim.opt.clipboard = "" -- dont save to clipboard
 vim.opt.conceallevel = 0
+
+vim.g.ai_cmp = false -- disable showing copilot complitions in blink, it will be shown with ghost text instead
+
+vim.g.snacks_animate = false -- stop scroll animation
 
 -- remove trailing whitespaces
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
