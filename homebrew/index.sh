aa274eeedc4a43631ec688416e97eb8d9838c0fd
timjackleus <timjackleus@gmail.com>
2025-03-06 09:55:06 +0100

    fix(homebrew): update paths in script

--- homebrew/index.sh
+++ homebrew/index.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # upgrade any already-installed formulae
 brew upgrade
 
 # Run all installations
-./brew.sh
-./brew-cask.sh
+"$SCRIPT_DIR/brew.sh"
+"$SCRIPT_DIR/brew-cask.sh"
 
 # remove outdated versions from the cellar
 brew cleanup
