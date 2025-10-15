72cfaf52c374132642da1d8324cbd4ccd13d4035
Tim Jackleus <timjackleus@gmail.com>
2025-10-15 11:23:21 +0200

    fix(ghostty): add rose-pine theme files

--- ghostty/setup.sh
+++ ghostty/setup.sh
@@ -1,3 +1,4 @@
 # Symlink config (will force if file already exist)
 mkdir -p ~/.config/ghostty
 ln -s $PWD/ghostty/config ~/.config/ghostty/config
+ln -s $PWD/ghostty/themes ~/.config/ghostty/themes
